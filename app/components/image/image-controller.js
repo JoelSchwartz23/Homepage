05aac196c9402eb5361103d56414db56b2708e0e
Joel Schwartz <nederlands18@gmail.com>
2019-03-09 15:30:58 -0700

    push

---
+++ app/components/image/image-controller.js
@@ -0,0 +1,15 @@
+import ImageService from "./image-service.js";
+
+let _imageService = new ImageService()
+
+function drawImage(images) {
+  console.log(images)
+  document.body.style.backgroundImage = `url(${images.url})`
+}
+
+
+export default class ImageController {
+  constructor() {
+    _imageService.getImage(drawImage)
+  }
+}
---
+++ app/components/quote/quote-controller.js
@@ -0,0 +1,41 @@
+import QuoteService from "./quote-service.js";
+
+let _qs = new QuoteService
+
+function drawQuote(quotes) {
+  console.log(quotes)
+  let Template = `<p> <b>Author:</b> <b>${quotes.author}</b> | <b>Quote of the day:</b> "${quotes.body}"</p>`
+  document.getElementById("quote").innerHTML = Template
+}
+
+function getTime() {
+  let date = new Date()
+  let hours = date.getHours()
+  let strHours = hours % 12 > 0 ? hours % 12 : hours
+  let minutes = date.getMinutes()
+  let strMinutes = minutes.toString()
+  if (strMinutes.length == 1) {
+    strMinutes = '0' + strMinutes
+  }
+  let seconds = date.getSeconds()
+  let strSeconds = seconds.toString()
+  if (strSeconds.length == 1) {
+    strSeconds = '0' + strSeconds
+  }
+  let tod = Math.floor(hours / 12) > 0 ? "PM" : "AM"
+
+  let time = (hours % 12) + ":" + strMinutes + ":" + strSeconds + " " + tod;
+
+  document.getElementById("time").innerText = time;
+  document.getElementById("time").textContent = time;
+}
+getTime();
+setInterval(getTime, 1000);
+
+export default class QuoteController {
+  constructor() {
+    _qs.getQuote(drawQuote)
+
+
+  }
+}
