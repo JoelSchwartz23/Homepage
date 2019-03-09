20d33e69b1d35ef66f32b00b375dcb89216a8646
Joel Schwartz <nederlands18@gmail.com>
2019-03-09 15:28:57 -0700

    push

---
+++ app/components/todo/todo-controller.js
@@ -0,0 +1,70 @@
+import TodoService from "./todo-service.js";
+
+var todoService = new TodoService
+
+// Use this getTodos function as your callback for all other edits
+function getTodos() {
+  //FYI DONT EDIT ME :)
+  todoService.getTodos(drawTodo)
+}
+
+
+function drawTodo(todos) {
+  //WHAT IS MY PURPOSE?
+  //BUILD YOUR TODO TEMPLATE HERE
+  console.log(todos)
+  var template = `<b> To do list : </b>  <ins>${todos.length}</ins>`
+
+  //DONT FORGET TO LOOP
+  todos.forEach(todo => {
+    // let checked = ""
+    // if (todo._completed) {
+    // 	checked = "checked"
+    // 
+    template += `
+			<li>
+				<input type="checkbox" ${todo.completed ? "checked" : ""}  onchange="app.controllers.todoController.toggleTodoStatus('${todo._id}')" id="${todo._id}"> -- ${todo.description}<i class="fa fa-fw fa-trash action muted" onclick="app.controllers.todoController.deleteTodo('${todo._id}')"></i>
+			</li>
+		`
+  });
+  document.getElementById('todo').innerHTML = template;
+}
+
+
+export default class TodoController {
+  constructor() {
+    // IF YOU WANT YOUR TODO LIST TO DRAW WHEN THE PAGE FIRST LOADS WHAT SHOULD YOU CALL HERE???
+    todoService.getTodos(drawTodo)
+  }
+
+  addTodoFromForm(e) {
+    e.preventDefault() // <-- hey this time its a freebie don't forget this
+    // TAKE THE INFORMATION FORM THE FORM
+    var form = e.target;
+    var todo = {
+      description: form.description.value
+      // DONT FORGET TO BUILD YOUR TODO OBJECT
+    }
+    //PASSES THE NEW TODO TO YOUR SERVICE
+    //DON'T FORGET TO REDRAW THE SCREEN WITH THE NEW TODO
+    //YOU SHOULDN'T NEED TO CHANGE THIS
+    todoService.addTodo(todo, getTodos)
+    //^^^^^^^ EXAMPLE OF HOW TO GET YOUR TOODOS AFTER AN EDIT
+  }
+
+  toggleTodoStatus(todoId) {
+    // let todo = {
+    // 	_id: todoid,
+    // 	completed: document.getElementById('todoid')
+    // }
+    todoService.toggleTodoStatus(todoId, getTodos)
+    // asks the service to edit the todo statuodos)
+    // YEP THATS IT FOR ME
+  }
+
+  deleteTodo(todoId) {
+    // ask the service to run the remove todo with this id
+    todoService.deleteTodo(todoId, drawTodo)
+    // ^^^^ THIS LINE OF CODE PROBABLY LOOKS VERY SIMILAR TO THE toggleTodoStatus
+  }
+}
---
+++ app/components/todo/todo-service.js
@@ -0,0 +1,62 @@
+// @ts-ignore
+const todoApi = axios.create({
+  baseURL: 'https://bcw-sandbox.herokuapp.com/api/Joels/todos/',
+  timeout: 3000
+});
+
+function logError(e) {
+  throw new Error(e)
+}
+
+
+let todoList = []
+
+export default class TodoService {
+
+  getTodos(draw) {
+    console.log("Getting the Todo List")
+    todoApi.get('')
+      .then((res) => { // <-- WHY IS THIS IMPORTANT????
+        todoList = res.data.data
+        draw(todoList)
+      })
+      .catch(logError)
+  }
+
+  addTodo(todo, getTodoscb) {
+    // WHAT IS THIS FOR???
+    todoApi.post('', todo)
+      .then(function (res) { // <-- WHAT DO YOU DO AFTER CREATING A NEW TODO?
+        getTodoscb()
+      })
+      .catch(logError)
+  }
+
+  deleteTodo(todo, getTodoscb) {
+    todoApi.delete(todo)
+      .then(res => {
+        this.getTodos(getTodoscb)
+      })
+      .catch(logError)
+  }
+
+  toggleTodoStatus(todoId, getTodos) {
+    // MAKE SURE WE THINK THIS ONE THROUGH
+    //STEP 1: Find the todo by its index **HINT** todoList
+    let todo = {}
+    for (let i = 0; i < todoList.length; i++) {
+      let t = todoList[i];
+      if (todoId == t._id) {
+        todo = t;
+      }
+    }
+    todo.completed = !todo.completed;
+
+    todoApi.put(todoId, todo)
+      .then(function (res) {
+        getTodos()
+      })
+      .catch(logError)
+  }
+
+}
